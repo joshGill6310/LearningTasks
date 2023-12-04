e129ad287c69a1b89d56b258dab99561b9c69e73
joshGill6310 <71409250+joshGill6310@users.noreply.github.com>
2023-12-04 15:32:34 -0500

    Added Sidebar feature and updated app to utilize it

--- src/Components/ProjectSideBar.jsx
+++ src/Components/ProjectSideBar.jsx
@@ -1,8 +1,10 @@
 export default function ProjectSideBar(){
     return(
-        <aside>
-            <h2>Your Projects</h2>
-            <button>+ Add Projects</button>
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2 className="mb-8 font-bold uppercase mb:text-xl text-stone-200">Your Projects</h2>
+            <div>
+                <button className="px-4 py-2">+ Add Projects</button>
+            </div>
             <ul>
                 
             </ul>
