8ce27b722dda5336583a488efa00af2f8699489a
Phoenix <71409250+joshGill6310@users.noreply.github.com>
2023-12-04 11:50:01 -0500

    first commit

---
+++ src/Components/ProjectSideBar.jsx
@@ -0,0 +1,12 @@
+export default function ProjectSideBar(){
+    return(
+        <aside>
+            <h2>Your Projects</h2>
+            <button>+ Add Projects</button>
+            <ul>
+                
+            </ul>
+        </aside>
+
+    );
+}
